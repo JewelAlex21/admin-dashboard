0d14617476301da66bdaed34cd3aa0a55bcf4d4e
JewelAlex21 <jewelalex2103@gmail.com>
2022-10-11 18:32:26 +0530

    dashboard-widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
+import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Widget  from "../../components/widget/Widget"
 import "./home.scss"
 
 const Home = () => {
@@ -6,7 +8,14 @@ const Home = () => {
     <div className="home">
         <Sidebar/>
         <div className="homeContainer">
-          container
+          <Navbar/>
+          <div className="widgets">
+            <Widget type="user" />
+            <Widget type="order"/>
+            <Widget type="earning"/>
+            <Widget type="balance"/>
+
+          </div>
         </div>
     </div>
   )
