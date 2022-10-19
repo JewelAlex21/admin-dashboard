260fd06e5b33afdf3ad838e10caad6744fc4bdb4
JewelAlex21 <jewelalex2103@gmail.com>
2022-10-19 13:07:12 +0530

    firebasestorage

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -13,4 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);
